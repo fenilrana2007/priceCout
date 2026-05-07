279b58db0783fbc9ff8661b794bb5ae6242fe197
fenilrana2007 <ranafenil3307@example.com>
2026-05-07 21:15:20 +0530

    all done now

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ app.use(cors());
   app.use(cors({
    origin: [
       "http://localhost:3000",
-      "https://friendly-cranachan-2fbc8b.netlify.app"
+      "https://price-out.netlify.app"
     ],
 }));
   // Health check
