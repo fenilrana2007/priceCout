8685c71dc7abb10f0221d8de3be3be1a30e78775
fenilrana2007 <ranafenil3307@example.com>
2026-05-08 11:35:02 +0530

    meta

---
+++ android/app/src/main/java/com/priseOut/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.priseOut.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.priseOut.app',
+  appName: 'priseOut',
+  webDir: 'dist'
+};
+
+export default config;
